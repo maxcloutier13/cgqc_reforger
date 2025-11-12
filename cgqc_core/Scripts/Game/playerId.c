a39dc08d5c6a7a1817f7ecc1dda97926212f9bc8
Maxime Cloutier <mcloutier13@gmail.com>
2025-11-12 16:01:17 -0500

    Ajout: Cloutier cigs

--- cgqc_core/Scripts/Game/playerId.c
+++ cgqc_core/Scripts/Game/playerId.c
@@ -7,6 +7,7 @@ modded class SCR_PlayerController : PlayerController
         // Check if we're taking control of a new entity (player spawn)
         if (to)
         {
+			PrintFormat("[CGQC_TakingControl] Waiting %1s", 5);
             // Wait 5 seconds before running initialization code
             GetGame().GetCallqueue().CallLater(InitializePlayer, 5000, false, to);
         }
@@ -15,35 +16,32 @@ modded class SCR_PlayerController : PlayerController
     // Initialize player after delay
     void InitializePlayer(IEntity playerEntity)
     {
+        Print("[CGQC_InitializePlayer] Starting ->");
         // Get player's Steam ID
         int playerId = GetPlayerId();
         string steamId = GetGame().GetBackendApi().GetPlayerIdentityId(playerId);
-
         // Get player's name
         string playerName = GetPlayerName();
-
-        
 
         // Check Steam ID and activate specific features for certain players
         switch (steamId)
         {
             case "d7e9113c-f075-41c5-a72a-9ee5187dc723": // Cloutier
             {
-                Print("[Special] Cloutier detected - activating admin features");
+                Print("[CGQC_InitializePlayer] Cloutier detected - activating admin features");
                 ActivateAdminFeatures(playerEntity);
-				string message = string.Format("Cloutier sti!");
-		        SCR_HintManagerComponent.GetInstance().ShowCustomHint(message, "Master", 10.0);
                 break;
             }
             default:
             {
-                Print(string.Format("[Welcome] Regular player %1 (Steam ID: %2) has spawned", playerName, steamId));
+                PrintFormat("[CGQC_InitializePlayer] Unrecognized player %1 (Steam ID: %2) has spawned", playerName, steamId);
 				// Display welcome message
 		        string message = string.Format("Salut, %1!", playerName);
 		        SCR_HintManagerComponent.GetInstance().ShowCustomHint(message, "Bienvenue", 10.0);
                 break;
             }
         }
+        Print("[CGQC_InitializePlayer] Done <-");
     }
 
     // Helper method to get player name
@@ -56,8 +54,32 @@ modded class SCR_PlayerController : PlayerController
     // Admin features activation
     void ActivateAdminFeatures(IEntity playerEntity)
     {
-
+		// Cig time
+		GiveItemToPlayer(playerEntity, "{F723BDF891EFECAE}Prefabs/Items/Smokeables/Smokeable_Joint.et");
+		GiveItemToPlayer(playerEntity, "{494F9EF9C6700796}Prefabs/Items/Smokeables/Smokeable_Cigarette.et");
+        GiveItemToPlayer(playerEntity, "{33CBDE73AB48172A}Prefabs/Weapons/Explosives/DemoBlock_M112/DemoBlock_M112.et");
         SCR_HintManagerComponent.GetInstance().ShowCustomHint("Admin mode activated", "System", 3.0);
+		string message = string.Format("Cloutier sti!");
+		SCR_HintManagerComponent.GetInstance().ShowCustomHint(message, "Master", 10.0);
+    }
+
+	// Give item to player
+	void GiveItemToPlayer(IEntity playerEntity, string itemToAdd)
+    {
+        PrintFormat("[CGQC_GiveItemToPlayer] called for item: %1", itemToAdd);
+
+        SCR_InventoryStorageManagerComponent inventoryManager = SCR_InventoryStorageManagerComponent.Cast(playerEntity.FindComponent(SCR_InventoryStorageManagerComponent));
+
+        if (!inventoryManager)
+        {
+            PrintFormat("[CGQC_GiveItemToPlayer] ERROR: No inventory manager");
+            return;
+        }
+
+        // Let the system choose the best storage automatically by passing null
+        bool success = inventoryManager.TrySpawnPrefabToStorage(itemToAdd, null, -1, EStoragePurpose.PURPOSE_ANY);
+
+        PrintFormat("[CGQC_GiveItemToPlayer] Result: %1", success);
     }
 }
 
