e30485ee6309c55a38d64468c139bf792049e47e
Maxime Cloutier <mcloutier13@gmail.com>
2025-12-03 18:31:30 -0500

    Fix: FF init when GM-ing

--- cgqc_ff/Scripts/Game/playerInit.c
+++ cgqc_ff/Scripts/Game/playerInit.c
@@ -4,13 +4,24 @@ modded class SCR_PlayerController : PlayerController
     {
         super.OnControlledEntityChanged(from, to);
 
-        // Check if we're taking control of a new entity (player spawn)
-        if (to)
-        {
-			Print("[CGQC_FF_TakingControl] Waiting a bit");
-			int playerId = GetPlayerId();
-            GetGame().GetCallqueue().CallLater(CGQC_Scripts.initializePlayer, 10000, false, to, playerId);
-            GetGame().GetCallqueue().CallLater(CGQC_Scripts.initializeFFPlayer, 6000, false, to);
-        }
+    	if (!to)
+			return;
+		
+		// Only run on server
+		if (!Replication.IsServer())
+			return;
+		
+		// Check if the controlled entity is the player's main entity (not a possessed unit)
+		IEntity mainEntity = GetMainEntity();
+		if (to != mainEntity)
+		{
+			Print("[CGQC_OnControlledEntityChanged] Possessing another entity, skipping initialization");
+			return;
+		}
+			
+		Print("[CGQC_FF_TakingControl] Waiting a bit");
+		int playerId = GetPlayerId();
+        GetGame().GetCallqueue().CallLater(CGQC_Scripts.initializeFFPlayer, 10000, false, to, playerId);
+        
     }
 }
