53848a89e0fd45b6ded6e47c90b7078ab1d176a4
Maxime Cloutier <mcloutier13@gmail.com>
2025-12-03 18:30:36 -0500

    Fix: run only on server and switch faces

--- cgqc_core/Scripts/Game/CGQC_IdentifyPlayers.c
+++ cgqc_core/Scripts/Game/CGQC_IdentifyPlayers.c
@@ -1,5 +1,36 @@
 modded class SCR_PlayerController : PlayerController
 {
+	
+	override void OnControlledEntityChanged(IEntity from, IEntity to)
+	{
+		super.OnControlledEntityChanged(from, to);
+
+		if (!to)
+			return;
+		
+		// Only run on server
+		if (!Replication.IsServer())
+			return;
+		
+		// Check if the controlled entity is the player's main entity (not a possessed unit)
+		IEntity mainEntity = GetMainEntity();
+		if (to != mainEntity)
+		{
+			Print("[CGQC_OnControlledEntityChanged] Possessing another entity, skipping initialization");
+			return;
+		}
+		
+		PrintFormat("[CGQC_OnControlledEntityChanged] Valid spawn for playerId: %1", GetPlayerId());
+		int playerId = GetPlayerId();
+		string playerIdentityId = SCR_PlayerIdentityUtils.GetPlayerIdentityId(playerId);
+		GetGame().GetCallqueue().CallLater(CGQC_Scripts.initializePlayer, 10000, false, to, playerId, playerIdentityId);
+	}
+}
+
+
+/*
+modded class SCR_PlayerController : PlayerController
+{
     override void OnControlledEntityChanged(IEntity from, IEntity to)
     {
         super.OnControlledEntityChanged(from, to);
@@ -8,9 +39,13 @@ modded class SCR_PlayerController : PlayerController
         if (to)
         {
 			PrintFormat("[CGQC_TakingControl] Waiting %1s", 10);
-            // Wait 5 seconds before running initialization code
+            // Wait 10 seconds before running initialization code
 			int playerId = GetPlayerId();
-            GetGame().GetCallqueue().CallLater(CGQC_Scripts.initializePlayer, 10000, false, to, playerId);
+			// Bohemia ID
+        	string playerIdentityId = SCR_PlayerIdentityUtils.GetPlayerIdentityId(playerId);
+			PrintFormat("[CGQC_identifyPlayer] playerId: %1 - playerIdentityId: %2 - playerName: %3", playerId, playerIdentityId);
+            GetGame().GetCallqueue().CallLater(CGQC_Scripts.initializePlayer, 10000, false, to, playerId, playerIdentityId);
         }
     }
 }
+*/
