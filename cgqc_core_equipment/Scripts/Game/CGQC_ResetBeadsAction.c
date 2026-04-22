82f828b9c7602be2df9eb8872c14b814f75a3d60
Maxime Cloutier <mcloutier13@gmail.com>
2026-04-22 01:28:32 -0400

    Ranger beads added reset

---
+++ cgqc_core_equipment/Scripts/Game/CGQC_ResetBeadsAction.c
@@ -0,0 +1,35 @@
+class CGQC_ResetBeadsAction : ScriptedUserAction
+{
+    override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
+    {
+        SCR_PaceDistanceComponent tracker = SCR_PaceDistanceComponent.Cast(
+            pUserEntity.FindComponent(SCR_PaceDistanceComponent)
+        );
+
+        if (!tracker)
+            return;
+
+        tracker.ResetDistance();
+
+        SCR_HintManagerComponent.GetInstance().ShowCustomHint(
+            "Pace count reset.",
+            "Ranger Beads",
+            2.0
+        );
+    }
+
+    override bool CanBeShownScript(IEntity user)
+    {
+        return true;
+    }
+
+    override bool HasLocalEffectOnlyScript()
+    {
+        return true;
+    }
+
+    override bool CanBePerformedScript(IEntity user)
+    {
+        return true;
+    }
+}
