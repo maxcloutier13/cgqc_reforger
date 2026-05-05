098e1e3afdce0a6d767858fc633809b9bd71d141
Maxime Cloutier <mcloutier13@gmail.com>
2026-05-05 15:56:05 -0400

    Fix tap

--- cgqc_addons_1.6/scripts/Game/CGQC_BON_TapShoulder.c
+++ cgqc_addons_1.6/scripts/Game/CGQC_BON_TapShoulder.c
@@ -6,41 +6,38 @@ modded class BON_TapShoulderAction : ScriptedUserAction
 		if (!Replication.IsServer())
 			return;
 
-		SCR_CharacterControllerComponent characterController = SCR_CharacterControllerComponent.Cast(pOwnerEntity.FindComponent(SCR_CharacterControllerComponent));
-		if (characterController)
-			characterController.AskCameraShake();
-
 		PlayerManager playerManager = GetGame().GetPlayerManager();
 		if (!playerManager)
-		{
 			return;
-		}
 
 		int tapperPlayerId = playerManager.GetPlayerIdFromControlledEntity(pUserEntity);
 		if (tapperPlayerId == 0)
 			return;
-		
+
 		string tapperName = CGQC_Scripts.GetCustomPlayerName(tapperPlayerId);
-		
 		if (tapperName.IsEmpty())
 			return;
 
 		int ownerPlayerId = playerManager.GetPlayerIdFromControlledEntity(pOwnerEntity);
-		
 		if (ownerPlayerId == 0)
 			return;
 
-		BON_TapShoulderReceiverComponent receiver = BON_TapShoulderReceiverComponent.Cast(pOwnerEntity.FindComponent(BON_TapShoulderReceiverComponent));
+		string ownerName = CGQC_Scripts.GetCustomPlayerName(ownerPlayerId);
+		if (ownerName.IsEmpty())
+			return;
 
+		BON_TapShoulderReceiverComponent receiver = BON_TapShoulderReceiverComponent.Cast(pOwnerEntity.FindComponent(BON_TapShoulderReceiverComponent));
 		if (!receiver)
 			return;
 
 		BON_TapShoulderReceiverComponent tapperReceiver = BON_TapShoulderReceiverComponent.Cast(pUserEntity.FindComponent(BON_TapShoulderReceiverComponent));
 
-		if (tapperReceiver)
-			tapperReceiver.ShowTapNotification("You tapped " + tapperName);
+		// Tapped player gets the shake + notification
+		receiver.ShowTapNotification(tapperName + " is on you", true);
 
-		receiver.ShowTapNotification(tapperName + " is on you");
+		// Tapper gets notification only
+		if (tapperReceiver)
+			tapperReceiver.ShowTapNotification("You tapped " + ownerName, false);
 	}
 }
 
@@ -48,19 +45,24 @@ class BON_TapShoulderReceiverComponentClass : ScriptComponentClass {}
 
 class BON_TapShoulderReceiverComponent : ScriptComponent
 {
-	void ShowTapNotification(string message)
+	void ShowTapNotification(string message, bool shake)
 	{
-		Rpc(RpcDo_ShowTapNotification, message);
+		Rpc(RpcDo_ShowTapNotification, message, shake);
 	}
 
 	[RplRpc(RplChannel.Reliable, RplRcver.Owner)]
-	void RpcDo_ShowTapNotification(string message)
+	void RpcDo_ShowTapNotification(string message, bool shake)
 	{
+		if (shake)
+		{
+			SCR_CharacterControllerComponent cc = SCR_CharacterControllerComponent.Cast(GetOwner().FindComponent(SCR_CharacterControllerComponent));
+			if (cc)
+				cc.AskCameraShake();
+		}
+
 		SCR_PopUpNotification popup = SCR_PopUpNotification.GetInstance();
 		if (!popup)
-		{
 			return;
-		}
 
 		popup.PopupMsg(message, 1.0);
 	}
