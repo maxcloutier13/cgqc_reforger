e8d5d6619eefb3b765c3b09ead78387ce300d3ad
Maxime Cloutier <mcloutier13@gmail.com>
2026-05-17 14:07:38 -0400

    IFAK no mo' other stuff

---
+++ cgqc_medical/Scripts/Game/CGQC_medicalStorageTag.c
@@ -0,0 +1,4 @@
+[ComponentEditorProps(category: "CGQC/Medical", description: "Marks this storage as medical-only. Blocks explosive items from being inserted via CanStoreItem.")]
+class CGQC_MedicalStorageTagClass : ScriptComponentClass {}
+
+class CGQC_MedicalStorageTag : ScriptComponent {}
