6de5ee99a9147c1e2a39ce6c8308140d91bc41fa
Maxime Cloutier <mcloutier13@gmail.com>
2026-05-19 20:20:02 -0400

    Move lacorne

--- cgqc_medical/Scripts/Game/CGQC_medicalStorageTag.c
+++
@@ -1,4 +0,0 @@
-[ComponentEditorProps(category: "CGQC/Medical", description: "Marks this storage as medical-only. Blocks explosive items from being inserted via CanStoreItem.")]
-class CGQC_MedicalStorageTagClass : ScriptComponentClass {}
-
-class CGQC_MedicalStorageTag : ScriptComponent {}
