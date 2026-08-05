35419964e978e0b1b09bf5ffbafd4862df3742dd
Maxime Cloutier <mcloutier13@gmail.com>
2026-08-04 23:16:57 -0400

    Added enum

--- cgqc_core/Scripts/Game/CGQC_ArsenalItemType.c
+++ cgqc_core/Scripts/Game/CGQC_ArsenalItemType.c
@@ -1,6 +1,6 @@
 modded enum SCR_EArsenalItemType
 {
-    CGQC_TRAINING_ITEM,
-	GRIT_ARMORY,
-	GRIT_ITEM
+	CGQC_TRAINING_ITEM = 1 << 25,	
+	GRIT_ARMORY        = 1 << 26,
+	GRIT_ITEM          = 1 << 27,
 }
